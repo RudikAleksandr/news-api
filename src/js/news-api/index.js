0b5abd6c9be766cdd0d025651513aba269db8489
RudikAleksandr <aleksandr.rudin.98@gmail.com>
2018-11-13 07:51:15 +0300

    Add NewsApi class

---
+++ src/js/news-api/index.js
@@ -0,0 +1,34 @@
+
+export default class NewsApi {
+  static get API_KEY() {
+    return '44e9b020293440f680fab34b192b8dd0';
+  }
+
+  static get URL_ALL_SOURCES() {
+    return 'https://newsapi.org/v1/sources';
+  }
+
+  static get URL_SOURCE() {
+    return 'https://newsapi.org/v2/top-headlines';
+  }
+
+  static get requestOptions() {
+    return {
+      method: 'GET',
+      headers: {
+        'X-Api-Key': this.API_KEY,
+      },
+    };
+  }
+
+  static httpGetAllSources() {
+    return fetch(this.URL_ALL_SOURCES, this.requestOptions)
+      .then(responce => responce.json());
+  }
+
+  static httpGetArticlesSource(idSource, numberArticles) {
+    const queryURL = `${this.URL_SOURCE}/?sources=${idSource}&pageSize=${numberArticles}`;
+    return fetch(queryURL, this.requestOptions)
+      .then(responce => responce.json());
+  }
+}
