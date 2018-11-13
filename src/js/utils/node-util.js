9eae9d72e2847f6d50348496ed003cbd4abb736f
RudikAleksandr <aleksandr.rudin.98@gmail.com>
2018-11-13 07:53:25 +0300

    Add node util

---
+++ src/js/utils/node-util.js
@@ -0,0 +1,30 @@
+
+function createNode({
+  tagName, innerText, innerHTML, className, attrs,
+}) {
+  const node = document.createElement(tagName);
+
+  if (className) {
+    node.classList.add(className);
+  }
+
+  if (innerText) {
+    node.innerText = innerText;
+  }
+
+  if (innerHTML) {
+    node.innerHTML = innerHTML;
+  }
+
+  if (attrs) {
+    Object.keys(attrs).forEach((attr) => {
+      node.setAttribute(attr, attrs[attr]);
+    });
+  }
+
+  return node;
+}
+
+export {
+  createNode,
+};
