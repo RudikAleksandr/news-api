d5cb62d5f86eefe891758026250327c4d11b62b3
RudikAleksandr <aleksandr.rudin.98@gmail.com>
2019-02-10 00:15:17 +0300

    Add facebook autharization

--- auth.js
+++ auth.js
@@ -1,8 +1,10 @@
 import passport from 'passport';
 import passportLocal from 'passport-local';
+import passportFacebook from 'passport-facebook';
 import * as DbUser from './db/user/user-db'; 
 
 const LocalStrategy = passportLocal.Strategy;
+const FacebookStrategy = passportFacebook.Strategy;
 
 passport.use(new LocalStrategy((username, password, done) => {
   DbUser.findUser({ username, password }, (err, user) => {
@@ -15,6 +17,29 @@ passport.use(new LocalStrategy((username, password, done) => {
     }
     
     return done(null, user);
+  });
+}));
+
+passport.use('facebook', new FacebookStrategy({
+  clientID: '712231772474627',
+  clientSecret: '14b2cba20c4dbbdf8c4b9a8a91d3ca45',
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+}, (access_token, refresh_token, profile, done) => {
+  DbUser.findUserById(profile.id, (err, user) => {
+    if (user) {
+      return done(null, user); 
+    } else {
+      console.log(profile);
+      const newUser = {
+        _id: profile.id,
+        username: profile.displayName, 
+        password: 'facebook',
+      };
+
+      DbUser.createUser(newUser, (err, user) => {
+        done(null, user);
+      });
+    } 
   });
 }));
 
--- routes/auth-router.js
+++ routes/auth-router.js
@@ -5,11 +5,28 @@ const router = express.Router();
 
 router.post('/login', 
   passport.authenticate('local', { 
-    failureRedirect: '/login',
     successRedirect: '/', 
+    failureRedirect: '/login',
 }));
 
 router.get('/login', (req, res, next) => {
   res.render('login');
 });
+
+router.get('/auth/facebook', 
+  passport.authenticate('facebook')
+);
+
+router.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', {
+    successRedirect : '/',
+    failureRedirect : '/login'
+  })
+);
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
 export default router;
