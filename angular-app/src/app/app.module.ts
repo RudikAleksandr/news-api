d1a102db2a6fee0a13c2b245a0d0d21d2f9dec78
RudikAleksandr <aleksandr.rudin.98@gmail.com>
2019-02-10 22:08:25 +0300

    Add comnponents for main page

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderAppComponent } from './header-app/header-app.component';
+import { NewsItemComponent } from './news-item/news-item.component';
+import { NewsListComponent } from './news-list/news-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderAppComponent,
+    NewsItemComponent,
+    NewsListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
